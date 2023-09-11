c8039532fbe611c05464f27f0193fd2bc4061185
Fabian Scharnböck <fabian.scharnboeck@t-online.de>
2023-09-11 12:30:45 +0200

    Refactoring: use Path

--- classes/Edgar_api.py
+++ classes/Edgar_api.py
@@ -1,12 +1,26 @@
+import pathlib
+
 import requests
 import json
 import pandas as pd
 import os
 
+file_path = pathlib.Path("./filings").absolute()
+
+
+def generate_cik_format(cik: str):
+    while len(cik) < 10:
+        cik = "0" + cik
+
+    return cik
+
 
 class EDGAR_API:
-
-    headers = {"user-agent": "Rene Nowotny renenowo98@gmail.com"}
+    user_agents_path = pathlib.Path(__file__).parent.parent.absolute() / "user_agent.csv"
+    user_agent = pd.read_csv(user_agents_path, sep=",", header=0)
+    user_agent_name = user_agent.at[0, "name"]
+    user_agent_mail = user_agent.at[0, "mail"]
+    headers = {f"user-agent": f"{user_agent_name} {user_agent_mail}"}
 
     """
     first choose ticker i.e SWKS for Skywork Solutions
@@ -16,8 +30,12 @@ class EDGAR_API:
     """
 
     def __init__(self) -> None:
+        self.df = None
+        self.cik = None
+        self.ticker = None
+        cik_map_path = "./maps/ticker-cik_map.txt"
         cik_map = pd.read_csv(
-            "../helpful_files/ticker-cik_map.txt", sep="\t", header=None
+            cik_map_path, sep="\t", header=None
         )
         cik_map.set_index(0, inplace=True)
         self.cik_map = cik_map
@@ -26,13 +44,13 @@ class EDGAR_API:
 
         if retries > 0:
             self.ticker = ticker
-            dir_path = "../fillings/" + str(ticker)
+            dir_path = pathlib.Path(file_path / str(ticker)).absolute()
             # create directory for all following files
-            if os.path.exists(dir_path) == False:
+            if not os.path.exists(dir_path):
                 os.makedirs(dir_path.lower())
 
             self.cik = self.cik_map[1][ticker.lower()]
-            cik = self.generate_cik_format(str(self.cik))
+            cik = generate_cik_format(str(self.cik))
             req = requests.get(
                 "https://data.sec.gov/submissions/CIK{cik}.json".format(cik=cik),
                 headers=self.headers,
@@ -41,16 +59,9 @@ class EDGAR_API:
             if req.status_code == 200:
                 return json.loads(req.text)
             else:
-                self.get_all_data(ticker, retries-1)
-        
+                self.get_all_data(ticker, retries - 1)
+
         return None
-        
-
-    def generate_cik_format(self, cik: str):
-        while len(cik) < 10:
-            cik = "0" + cik
-
-        return cik
 
     def fillings_data_to_dataframe(self, fillings: json):
         self.df = pd.DataFrame(fillings)
@@ -58,23 +69,16 @@ class EDGAR_API:
         return self.df
 
     def get_file(self, accession_number: str, primary_document: str, report_date: str):
-        file_path = (
-            "../fillings/"
-            + self.ticker.lower()
-            + "/"
-            + report_date
-            + "_"
-            + accession_number
-            + "_"
-            + primary_document
+        file_path_to_use = (
+                file_path / self.ticker.lower() / (report_date + "_" + accession_number + "_" + primary_document)
         )
 
-        if os.path.exists(file_path) == False:
+        if not os.path.exists(file_path_to_use):
             # https://www.sec.gov/ix?doc=/Archives/edgar/data/4127/0000004127-19-000049/fy1910k92719.htm
-            url = f"https://www.sec.gov/Archives/edgar/data/{self.cik}/{accession_number.replace('-','')}/{primary_document}"
+            url = f"https://www.sec.gov/Archives/edgar/data/{self.cik}/{accession_number.replace('-', '')}/{primary_document}"
             print(url)
             #  BEISPIEL für XML url = "https://www.sec.gov/Archives/edgar/data/4127/000000412721000058/swks-20211001_htm.xml"
-            url = f"https://www.sec.gov/Archives/edgar/data/{self.cik}/{accession_number.replace('-','')}/{str(primary_document).replace('.htm','_htm.xml')}"
+            url = f"https://www.sec.gov/Archives/edgar/data/{self.cik}/{accession_number.replace('-', '')}/{str(primary_document).replace('.htm', '_htm.xml')}"
 
             req = requests.get(url, headers=self.headers)
 
@@ -83,8 +87,8 @@ class EDGAR_API:
                 print(req.text)
                 exit()
 
-            with open(file_path, "w") as f:
+            with open(file_path_to_use, "w") as f:
                 f.writelines(req.text)
                 f.close()
 
-        return file_path
+        return file_path_to_use
