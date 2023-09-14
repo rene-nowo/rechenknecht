60b278fe7100aba4056ebb771fc54c464afeaee7
Fabian Scharnböck <fabian.scharnboeck@t-online.de>
2023-09-14 09:28:34 +0200

    Ch: Adjusted test to not use network

--- test/test_rechenknecht_beta.py
+++ test/test_rechenknecht_beta.py
@@ -8,7 +8,14 @@ import pandas.testing as pd_testing
 
 
 def test_foot_locker():
-    file_list, name, industry = search_edgar_data("fl")
+    file_list = [
+        (pathlib.Path(__file__).parent / "resources" / "filings" / "fl" / "2023-01-28_0000850209-23-000006_fl-20230128x10k.htm", "2023-01-28"),
+        (pathlib.Path(__file__).parent / "resources" / "filings" / "fl" / "2022-01-29_0000850209-22-000003_fl-20220129x10k.htm", "2022-01-29"),
+        (pathlib.Path(__file__).parent / "resources" / "filings" / "fl" / "2021-01-30_0000850209-21-000003_fl-20210130x10k.htm", "2021-01-30"),
+        (pathlib.Path(__file__).parent / "resources" / "filings" / "fl" / "2020-02-01_0000850209-20-000007_fl-20200201x10k.htm", "2020-02-01")
+    ]
+    name = "FOOT LOCKER, INC."
+    industry = "Retail-Shoe Stores"
     if file_list:
         expected: pd.DataFrame = pd.read_csv(pathlib.Path(__file__).parent / "resources" / "footlocker_expected.csv",
                                              index_col=0)
